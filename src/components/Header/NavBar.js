34829c9ebe6cfb39b75b16418641e8bb34f32ece
LaithJabali <laithkingjabali@gmail.com>
2023-05-16 16:33:22 +0300

    @LaithJabali Updated the code based on the Monitor feedback

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,31 +1,32 @@
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './Header.module.css';
 import NavBarResponsive from './NavBarResponsive';
 import FooterNavBarResponsive from '../Footer/FooterNavBarResponsive';
-import {buttons} from './data';
+import { buttons } from './data';
 
-const NavBar = ({ navLinks , isFooter }) => {
-const [isMobile, setIsMobile] = useState(false);
-const handleResize = () => {
-  setIsMobile(window.innerWidth < 1200);
-};
-useEffect(() => {
-  handleResize();
-window.addEventListener('resize', handleResize);
-  return () => {
-    window.removeEventListener('resize', handleResize);
+const NavBar = ({ navLinks, isFooter }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 1200);
   };
-}, []);
-if (isMobile && isFooter) {
-    return (
-      <FooterNavBarResponsive  navLinks ={navLinks} />
-    );
-} else if (isMobile && !isFooter) { 
-    return(
-          <NavBarResponsive navLinks ={navLinks} buttons={buttons}/>
-        )}
-return (
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (isMobile && isFooter) {
+    return <FooterNavBarResponsive navLinks={navLinks} />;
+  } else if (isMobile && !isFooter) {
+    return <NavBarResponsive navLinks={navLinks} buttons={buttons} />;
+  }
+
+  return (
     <nav className={style.headerNav}>
       <ul>
         {navLinks.map(({ to, text }) => (
@@ -37,4 +38,5 @@ return (
     </nav>
   );
 };
+
 export default NavBar;
--- src/components/Header/NavBarResponsive.js
+++ src/components/Header/NavBarResponsive.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
 import MyMenu from './MyMenu';
 import { Dropdown } from 'antd';
-import { BarsOutlined} from '@ant-design/icons';
-import style from './Header.module.css'
+import { BarsOutlined } from '@ant-design/icons';
+import style from './Header.module.css';
 
-const NavBarResponsive = ({navLinks , buttons}) => {
-const menu = ( <MyMenu navLinks={navLinks} buttons={buttons}/>);
-return (
-    <Dropdown overlay={menu} placement='bottomLeft' className={style.dropdown} >
-        <a href='/'  onClick={e => e.preventDefault()}>
-             <BarsOutlined />
-        </a>
+const NavBarResponsive = ({ navLinks, buttons }) => {
+  const menu = (<MyMenu navLinks={navLinks} buttons={buttons} />);
+  return (
+    <Dropdown overlay={menu} placement='bottomLeft' className={style.dropdown}>
+      <a href='/' onClick={e => e.preventDefault()}>
+        <BarsOutlined />
+      </a>
     </Dropdown>
-  )
-}
-export default NavBarResponsive
+  );
+};
+
+export default NavBarResponsive;
