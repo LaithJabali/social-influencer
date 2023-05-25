e6b9de8c2a02ce68673ebd9f0f759c99307ce675
LaithJabali <laithkingjabali@gmail.com>
2023-05-25 14:23:09 +0300

    Updated the code based on the Monitor feedback

--- src/components/Filter/PopContent.js
+++ src/components/Filter/PopContent.js
@@ -2,23 +2,21 @@ import React from 'react';
 import styles from './Filter.module.css';
 import { useFilterContext } from './FilterContext';
 
-const PopContent = ({filterOptions}) => {
+const PopContent = ({ filterOptions }) => {
   const { selectedFilter, setSelectedFilter } = useFilterContext();
-
-  const handleFilterChange = (value) => {
-    setSelectedFilter(value);
-  };
+  const handleFilterChange = (value) => setSelectedFilter(value);
 
   return (
     <div className={styles.filterOptions}>
       {filterOptions.map((option) => (
-        <span
+        <a
           key={option.value}
-          className={selectedFilter === option.value ? styles.activeOption : ''}
+          className={`${selectedFilter === option.value ? styles.activeOption : ''} ${styles.option}`}
           onClick={() => handleFilterChange(option.value)}
+          href={'#' + option.value}
         >
           {option.label}
-        </span>
+        </a>
       ))}
     </div>
   );
