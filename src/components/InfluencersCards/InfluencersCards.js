4e4df847b8bbc179699202049e74c3357c8e0381
LaithJabali <laithkingjabali@gmail.com>
2023-06-07 14:58:23 +0300

    Set the filter to work

--- src/components/InfluencersCards/InfluencersCards.js
+++ src/components/InfluencersCards/InfluencersCards.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { Row } from 'antd';
 import styles from './InfluencersCards.module.css';
 import Cards from './Cards';
+import { useFilterContext } from '../Filter/FilterContext';
 
 const InfluencersCards = ({ influencersData }) => {
+  const { selectedFilter, searchValue } = useFilterContext();
+
+  const filteredInfluencers = influencersData.filter((influencer) => {
+    if (selectedFilter === 'name') {
+      return influencer.name.includes(searchValue);
+    } else if (selectedFilter === 'interests') {
+      return influencer.description.toLowerCase().includes(searchValue.toLowerCase());
+    } else if (selectedFilter === 'price') {
+      return influencer.price <= parseInt(searchValue);
+    } else if (selectedFilter === 'followers') {
+      return influencer.followers.toLowerCase().includes(searchValue.toLowerCase());
+    }
+    return true;
+  });
+
   return (
     <div className={styles.container}>
       <Row>
-        {influencersData.map((influencer) => (
+        {filteredInfluencers.map((influencer) => (
           <Cards key={influencer.id} data={influencer} />
         ))}
       </Row>
--- src/components/pages/BrowseInfluencers.js
+++ src/components/pages/BrowseInfluencers.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Filter from '../Filter/Filter'
 import style from './Pages.module.css'
-import Influencers from '../InfluencersCards/Influencers'
 
 const BrowseInfluencers = () => {
   return (
     <div className={style.container}>
       <Filter />
-      <Influencers />
     </div>
   )
 }
